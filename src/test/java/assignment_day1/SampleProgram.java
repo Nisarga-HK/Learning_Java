fde3167203ce0f83d387a079c5606cc29d14c1be
Nisarga-HK <nisargagowda543@gmail.com>
2023-07-25 10:02:33 +0530

    AddingNewProject

---
+++ src/test/java/assignment_day1/SampleProgram.java
@@ -0,0 +1,8 @@
+package assignment_day1;
+
+public class SampleProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Nisarga");
+    }
+}
---
+++ src/test/java/assignment_day1/SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+package assignment_day1;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1=4;
+        int num2=36;
+
+        int sum=num1+num2;
+        System.out.println("The Sum of two number " + num1 + " and " + num2 + " is :" + sum);
+    }
+}
